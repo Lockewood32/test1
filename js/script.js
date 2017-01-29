c9fb07475accdf6d6b5ce74633e00717e63ab9ed
Lockewood32 <lockewood32@gmail.com>
2017-01-29 16:20:14 -0500

    Update script.js

--- js/script.js
+++ js/script.js
@@ -2,14 +2,20 @@
 
 var content=[];
 
+var headingIndex = 0;
 for (cellData in testData) {
   document.getElementById("cellNameTemplate").innerText = testData[cellData]["name"];
   document.getElementById("cellImageTemplate").className = ("menu-sprite b"+ cellData);
   document.getElementById("cellNumberTemplate").innerText = testData[cellData]["num"];
   var cellTemplate = document.getElementById("cellTemplate").outerHTML;
   content.push(cellTemplate);
+  
+  headingIndex++;
+  if (headingIndex >= 30){
+    headingIndex = 0;
+    content.push(document.getElementById("headingTemplate").outerHTML);
+  }
 }
-content.push(document.getElementById("headingTemplate").outerHTML);
 content.push(document.getElementById("spacerTemplate").outerHTML);
 content = content.join("");
 
