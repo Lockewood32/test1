7b1acb7018c8b989adc0bd1be37a0e3a1d094233
Lockewood32 <lockewood32@gmail.com>
2017-01-29 16:03:41 -0500

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,21 @@
+﻿//console.log(testData);
+
+var content=[];
+
+for (cellData in testData) {
+  document.getElementById("cellNameTemplate").innerText = testData[cellData]["name"];
+  document.getElementById("cellImageTemplate").className = ("menu-sprite b"+ cellData);
+  document.getElementById("cellNumberTemplate").innerText = testData[cellData]["num"];
+  var cellTemplate = document.getElementById("cellTemplate").outerHTML;
+  content.push(cellTemplate);
+}
+content.push(document.getElementById("headingTemplate").outerHTML);
+content.push(document.getElementById("spacerTemplate").outerHTML);
+content = content.join("");
+
+document.getElementById('content').innerHTML = content;
+
+
+
+
+
